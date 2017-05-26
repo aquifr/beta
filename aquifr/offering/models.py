2ef93acc2de0b8c74759f827c002948206ecb228
Finn Terdal <fterdal@gmail.com>
2017-05-26 15:16:26 -0700

    Create model for Offering, migrations run for database.

--- aquifr/offering/models.py
+++ aquifr/offering/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Offering(models.Model):
+    title = models.CharField(max_length=200)
+    short_desc = models.TextField()
+    long_desc = models.TextField()
+    
+
+    def __str__(self):
+        return self.title
+
